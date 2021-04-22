763d13e1359ba34967c4672a82fad7c7c41db6f9
Marcin Kubiczek <Kubiczek.M@eplan.pl>
2021-04-22 07:49:25 +0200

    Update Test Case

--- Features/SimpleTest.js
+++ Features/SimpleTest.js
@@ -1,26 +1,30 @@
-const {chromium}=require('playwright');
+const {chromium} = require('playwright');
 
-(async()=>{
+(async () => {
     //inside the block
     const browser = await chromium.launch({
-        headless:false
+        headless: false
     });
-    const context= await browser.newContext({
+    const context = await browser.newContext({
         viewport: {
             width: 1920,
             height: 1080,
         },
-        ignoreHTTPSErrors:true
+        ignoreHTTPSErrors: true
     })
-    const page = context.newPage();
-    (await page).goto("https://www.phptravels.net/home");
-   // await page.goto("https://www.phptravels.net/home");
+    const page = await context.newPage();
+    //GO to page
+    await page.goto("http://automationpractice.com/index.php");
+    // Focus on element to get dropdown menu
+    await page.focus('a[title="Women"]')
+    //click on a Element
+    await page.click('text=Casual Dresses');
 
-    //Email user@phptravels.com
-    // Password demouser
-    (await page).click('//*[@id="dropdownCurrency"]');
-    (await page).click('//*[@id="//header-waypoint-sticky"]/div[1]/div/div/div[2]/div/ul/li[3]/div/div/div/a[1]')
-    await (await page).screenshot({path:'ea.png'});
+    //click on quick view
+    await page.click('img[title="Printed Dress"]');
+    //Double the quantity
+    await page.click('i[class="icon-plus"]');
+    await page.screenshot({path: 'ea.png'});
 
     await browser.close();
 })();
